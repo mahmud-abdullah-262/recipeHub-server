2d377ffde3be05ba9b20184088716f2ee9cba3ac
mahmud-abdullah-262 <citclassmahmud@gmail.com>
2026-06-20 00:17:05 +0600

    server setup done with token  verification

---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+const cors = require('cors');
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_CONNECTION;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
+
+    const db = client.db('recipe-hub-server')
+    const recipeCollection = db.collection('recipe-collection')
+  
+
+
+
+    // verification related 
+    const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+  const token = authHeader.split(' ')[1]
+   if(!token){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+
+  const query = {token: token}
+  const session = await sessionCollection.findOne(query)
+
+  if(!session) {
+      return res.status(403).send({message: 'forbidden'})
+  }
+  const userQuery = {_id : session.userId}
+  const user = await userCollection.findOne(userQuery)
+  req.user = user
+next()
+}
+
+
+
+    app.get('/', (req, res) => {
+      res.send('recipe hub server is running')
+    })
+ 
+
+
+
+    // all recipe fetching
+    app.get('/api/recipes', async (req, res) => {
+    
+     
+
+
+
+     
+      const cursor = recipeCollection.find();
+      const recipes = await cursor.toArray();
+      res.json(recipes);
+    }
+)
+
+// recipe by id fetching
+ app.get('/api/recipes/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      
+      const query = {_id: new ObjectId(id)} // ডাটায় যদি ম্যানুয়াল আইডি সেট করা থাকে তাহলে এভাবে। আর যদি ম্যানুয়ালি আইডি না থাকে তাহলে এভাবে - new ObjectId(id)
+
+      const result = await recipeCollection.findOne(query);
+      console.log(result, 'recipe')
+      res.json(result)
+    })
+
+// // job posting 
+//     app.post('/api/jobs', verifyToken, verifyRecruiter, async (req, res) => {
+//       const job = req.body;
+//       const newJob = {
+//         ...job,
+//         createdAt: new Date()
+//       }
+//       console.log("Received:", newJob);
+//       const result = await jobCollection.insertOne(newJob);
+//       res.json({ insertedId: result.insertedId.toString() })
+//     })
+
+
+
+
+
+  
+
+
+
+  // // job details data fetching
+  // app.get('/api/jobs/:id', async (req, res) => {
+  //   const id = req.params.id;
+  //   const query = {_id : new ObjectId(id)}
+  //   const result = await jobCollection.findOne(query)
+  //   res.json(result)
+    
+  // })
+
+ 
+
+
+
+
+  } catch(err) {
+    console.error(err);
+    
+  }
+}
+
+ 
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`recipe hub server is running in ${port}`)
+})
